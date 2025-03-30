7fa26216888d151db52ecf807a33645496a8c886
4Surpr1se <99213922+4Surpr1se@users.noreply.github.com>
2025-03-31 00:56:07 +0300

    adding <>

---
+++ stack.py
@@ -0,0 +1,20 @@
+class Stack:
+    """
+    Реализация стека на базе вектора
+    (для языка Python она тривиальна)
+    """
+
+    def __init__(self):
+        self.array = []
+
+    def push(self, c):
+        self.array.append(c)
+
+    def pop(self):
+        return self.array.pop()
+
+    def top(self):
+        return self.array[len(self.array) - 1]
+
+    def is_empty(self):
+        return len(self.array) == 0
